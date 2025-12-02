ed3db0d63a22f8cbc755034f183e1e026869751c
Josh <josh@Joshs-MacBook-Air.local>
2025-12-02 15:14:58 +1300

    Create profile for tile generation

--- planetiler/src/main/java/com/eroad/NzProfile.java
+++ planetiler/src/main/java/com/eroad/NzProfile.java
@@ -20,10 +20,48 @@ public class NzProfile implements Profile{
 
         String myType = sourceFeature.getString("type");
 
-        FeatureCollector.Feature feature = features.polygon(myType).setBufferPixels(4)
-            .setMinZoom(0)
-            .setMaxZoom(14);      
+        try{
+            if(myType != null && myType.equals("geofence")){
+                features.polygon("geofences")
+                .setBufferPixels(4)
+                .setMinZoom(0)
+                .setMaxZoom(14)
+                .setAttr("id", sourceFeature.getString("id"));
 
+            }
+            else if(myType != null && myType.equals("trip")){
+                features.line("trips")
+                .setBufferPixels(4)
+                .setMinZoom(0)
+                .setMaxZoom(14)
+                .setAttr("id", sourceFeature.getString("id"))
+                .setAttr("vehicle", sourceFeature.getString("vehicle"));
+            }
+            else if(myType != null && myType.equals("stop")){
+                features.point("stops")
+                .setBufferPixels(4)
+                .setMinZoom(0)
+                .setMaxZoom(14)
+                .setAttr("id", sourceFeature.getString("id"))
+                .setAttr("duration_mins", sourceFeature.getLong("duration_mins"));
+            }
+            else{
+                System.out.println("Unknown feature type:" + myType);
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error processing feature: " + e.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Planetiler.create(Arguments.fromArgs(args))
+            .setProfile(new NzProfile())
+            .addGeoJsonSource("nz_source", Path.of("../dataGeneration/data/sampleData.geojson"))
+            .overwriteOutput("outputTiles")
+            .run();
+            
     }
 
     
